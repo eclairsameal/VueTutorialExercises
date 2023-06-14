087252cdfa3e8288bff1c7bde7028624226bbf85
Fenrir <skolleclair@gmail.com>
2023-06-14 15:42:17 +0900

    Use vue to manage data and rewrite html

---
+++ 02-first-vue-app/before/index.js
@@ -0,0 +1,10 @@
+// Playlist 當作我們整個應用的設定對象，可以提供html方法 數據 等動態的東西
+const Playlist = {
+    data() {
+      return {
+        playlist: ["YOLO", "about Us", "Unleash"],
+      };
+    },
+  };
+// Vue 會讀取 Playlist 裡的配置，並掛載到 id 為 app 的元素上
+Vue.createApp(Playlist).mount("#app");
