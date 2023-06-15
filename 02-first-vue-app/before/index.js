431bbe16705c188022c363ec0c5d521dd6395b2a
Fenrir <skolleclair@gmail.com>
2023-06-15 10:24:54 +0900

    filter list

--- 02-first-vue-app/before/index.js
+++ 02-first-vue-app/before/index.js
@@ -6,6 +6,17 @@ const Playlist = {
         searchTerm: "",    // 用來保存使用者的輸入
       };
     },
+    computed: { // 屬性為一個對象
+        filteredPlaylist() { // 屬性為一個function
+            if (this.searchTerm) {
+                return this.playlist.filter(song => 
+                    song.toLowerCase().includes(this.searchTerm.toLowerCase())
+                );
+            } else {
+                return this.playlist;
+            }
+        }
+    }
   };
 // Vue 會讀取 Playlist 裡的配置，並掛載到 id 為 app 的元素上
 Vue.createApp(Playlist).mount("#app");
