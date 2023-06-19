e895f805f116fa6bb172cda37f2b196750384ba4
Fenrir <skolleclair@gmail.com>
2023-06-19 15:35:04 +0900

    表單的事件監聽

---
+++ 03-ex/ex_07_form_input/index.js
@@ -0,0 +1,14 @@
+// 範例：表單輸入控制事件處理
+const app = Vue.createApp({
+  data() {
+    return {
+      username: "",
+    };
+  },
+  methods: {
+    handleInput(e) {
+      this.username = e.target.value;
+    },
+  },
+});
+app.mount("#app");
