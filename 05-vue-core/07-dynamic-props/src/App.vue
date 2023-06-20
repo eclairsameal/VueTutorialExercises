32628ba3db604c25571d76cdd2e9d5e77236a628
Fenrir <skolleclair@gmail.com>
2023-06-20 18:00:31 +0900

    複製上一個章節的專案

---
+++ 05-vue-core/07-dynamic-props/src/App.vue
@@ -0,0 +1,48 @@
+<script>
+import MessageItem from './components/MessageItem.vue';  // 導入組件
+export default {
+  components: {
+    MessageItem
+  }
+};
+</script>
+
+<template>
+  <main>
+    <div>
+      <MessageItem sender="Max" msg="Hello" time="19:30"/>
+      <MessageItem sender="Jone" msg="Hi" time="21:40"/>
+      <MessageItem sender="Alex" msg="Hiiiii" time="22:27"/>
+    </div>
+  </main>
+</template>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  font-family: Arial, "PingFang SC", "Microsoft Yahei", sans-serif;
+}
+
+body {
+  background-color: #0f141c;
+  opacity: 1;
+  background-image: radial-gradient(#212943 0.6000000000000001px,
+      #0f141c 0.6000000000000001px);
+  background-size: 12px 12px;
+  color: white;
+}
+
+#app {
+  width: 100vw;
+  height: 100vh;
+  max-width: 100%;
+  display: grid;
+  place-items: center;
+}
+
+h1 {
+  margin-bottom: 2em;
+}
+</style>
---
+++ 05-vue-core/07-dynamic-props/src/components/MessageItem.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <p>{{ sender }}</p>
+    <span>{{ time }}</span>
+    <p>{{ msg }}</p>
+  </div>
+</template>
+<!--props屬性：自定義組件內容-->
+<script>
+export default {
+  props: ["sender", "msg", "time"],
+};
+</script>
+<style scoped>
+div {
+  display: grid;
+  grid-template-areas:
+    "sender time"
+    "msg msg";
+  border: 1px solid hsl(280deg, 50%, 50%);
+  border-radius: 4px;
+  padding: 1em;
+  width: 250px;
+}
+
+span {
+  justify-self: end;
+}
+
+div>p:last-child {
+  margin-top: 1em;
+}
+</style>
