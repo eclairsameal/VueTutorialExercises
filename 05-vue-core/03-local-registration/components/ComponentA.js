1f87b93327dea0ab720b43c4aea4a77b5b8deeb7
Fenrir <skolleclair@gmail.com>
2023-06-20 15:43:04 +0900

    局部註冊組件的範例

---
+++ 05-vue-core/03-local-registration/components/ComponentA.js
@@ -0,0 +1,3 @@
+export default {
+  template: `<p>我是组件 A</p>`,
+};
---
+++ 05-vue-core/03-local-registration/components/ComponentC.js
@@ -0,0 +1,5 @@
+import ComponentB from "./ComponentB.js";
+export default {
+  template: `<p>我是组件 C</p><ComponentB />`,
+  components: { ComponentB },
+};
